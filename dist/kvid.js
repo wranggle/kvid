6c5c627c5d80589c58a680e1373678101f7077bb
Jeff Ferber <jeff@proxv.com>
2017-06-19 13:51:39 +0700

    make standalone/independent package

--- dist/kvid.js
+++ dist/kvid.js
@@ -1,8 +1,7 @@
-// Copyright 2017 ProxV Inc.
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.kvid = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 "use strict";
 
-var _kvid = _dereq_(2);
+var _kvid = _dereq_("./src/kvid.js");
 
 var _kvid2 = _interopRequireDefault(_kvid);
 
@@ -15,7 +14,7 @@ Object.defineProperty(exports, "__esModule", {
 module.exports = _kvid2.default;
 module.exports.default = _kvid2.default;
 
-},{"2":2}],2:[function(_dereq_,module,exports){
+},{"./src/kvid.js":2}],2:[function(_dereq_,module,exports){
 (function (global){
 'use strict';
 
